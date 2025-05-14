496e52249771fb5bb6728bebd05c6a764637333f
Pedro Paulo <pedropaulo.bm2006@gmail.com>
2025-05-14 20:21:23 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import br.com.pedro.screenmatch.modelos.Movie;
+
+public class Main {
+  public static void main(String[] args) {
+    Movie filme = new Movie();
+    filme.setName("Projeto X");
+    filme.setYearOfRelease(2006);
+    filme.setDirector("Lebron James");
+  }
+}
---
+++ src/br/com/pedro/screenmatch/modelos/Title.java
@@ -0,0 +1,64 @@
+package br.com.pedro.screenmatch.modelos;
+
+public class Title {
+  private String name;
+  private int yearOfRelease;
+  private boolean included;
+  private double averageRating;
+  private int totalRating;
+  private int timeMovie;
+
+  public String getName() {
+    return name;
+  }
+
+  public int getYearOfRelease() {
+    return yearOfRelease;
+  }
+
+  public boolean isIncluded() {
+    return included;
+  }
+
+  public int getTimeMovie() {
+    return timeMovie;
+  }
+
+  public int getTotalRating() {
+    return totalRating;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setYearOfRelease(int yearOfRelease) {
+    this.yearOfRelease = yearOfRelease;
+  }
+
+  public void setIncluded(boolean included) {
+    this.included = included;
+  }
+
+  public void setTimeMovie(int timeMovie) {
+    this.timeMovie = timeMovie;
+  }
+
+  public double getAverageRating() {
+    return averageRating;
+  }
+
+  public void evaluate(double grade) {
+    averageRating+= grade;
+    totalRating++;
+  }
+
+  public double averageOfEvaluations() {
+    return averageRating / totalRating;
+  }
+
+  public void showTechnicalData() {
+    System.out.println("Nome do filme: " + name);
+    System.out.println("Ano de lançamento: " + yearOfRelease);
+  }
+}
